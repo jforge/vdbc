f047e149bc8035c6d55483381c360d93f6b1735b
Ivan Pavlov <ivnpav@gmail.com>
2014-10-03 13:20:13 +0400

    style fix

--- webapp/src/main/java/org/indp/vdbc/ui/metadata/SeparatedValuesStringDetailsProvider.java
+++ webapp/src/main/java/org/indp/vdbc/ui/metadata/SeparatedValuesStringDetailsProvider.java
@@ -1,14 +1,12 @@
 package org.indp.vdbc.ui.metadata;
 
 import com.vaadin.data.Item;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
+import org.indp.vdbc.ui.UiUtils;
 
-/**
- *
- *
- */
 public abstract class SeparatedValuesStringDetailsProvider implements DetailsProvider {
 
     private static final String VALUE_PROPERTY = "value";
@@ -34,16 +32,15 @@ public abstract class SeparatedValuesStringDetailsProvider implements DetailsPro
         }
 
         String[] values = value.split(separatorRegex);
-        Table t = new Table();
-        t.setSizeFull();
-        t.setSelectable(true);
-        t.setColumnHeaderMode(Table.ColumnHeaderMode.HIDDEN);
-        t.addContainerProperty(VALUE_PROPERTY, String.class, null);
-//        t.setColumnHeader(VALUE_PROPERTY, title);
+        IndexedContainer container = new IndexedContainer();
+        container.addContainerProperty(VALUE_PROPERTY, String.class, null);
         for (int i = 0; i < values.length; ++i) {
-            Item item = t.addItem(i);
+            Item item = container.addItem(i);
             item.getItemProperty(VALUE_PROPERTY).setValue(values[i].trim());
         }
+
+        Table t = UiUtils.createTable(container);
+        t.setColumnHeaderMode(Table.ColumnHeaderMode.HIDDEN);
         return t;
     }
 }
